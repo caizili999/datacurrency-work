c2b7e89d4d71720aa8cbf803b80d2e120869ed60
caizili2 <czl13709686057@163.com>
2023-04-01 23:27:28 +0800

    modified:   "\345\270\201\345\256\211\350\267\263\345\212\250\346\225\260\346\215\256\350\216\267\345\217\226.py"

--- 币安跳动数据获取.py
+++ 币安跳动数据获取.py
@@ -76,6 +76,7 @@ cookies2 = {
 }
 
 
+
 headers = {
     'authority': 'www.binance.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
